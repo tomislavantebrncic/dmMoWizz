bc38d1aad17ef691fad44552db9ea553635a5444
tomi77 <tomislav-ante.brncic@fer.hr>
2018-12-30 03:10:08 +0100

    Add project files.

---
+++ dmMoWizz/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(dmMoWizz.Startup))]
+namespace dmMoWizz
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
