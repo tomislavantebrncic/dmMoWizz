3a5d1027bc7e8005a3f327a4201990638671df8a
tomi77 <tomislav-ante.brncic@fer.hr>
2018-12-30 03:30:04 +0100

    Dodan FacebookController

---
+++ dmMoWizz/Controllers/FacebookController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dmMoWizz.Controllers
+{
+    [Authorize]
+    public class FacebookController : Controller
+    {
+        private ApplicationUserManager _userManager;
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+        // GET: Facebook
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> Posts()
+        {
+            var currentClaims = await UserManager.GetClaimsAsync(HttpContext.User.Identity.GetUserId());
+
+            var accessToken = currentClaims.FirstOrDefault(x => x.Type == "urn:tokens:facebook");
+
+            if (accessToken == null)
+            {
+                return (new HttpStatusCodeResult(HttpStatusCode.NotFound, "Token not found"));
+            }
+
+            string url = String.Format("https://graph.facebook.com/me?fields=id,name,gender&access_token={0}", accessToken);
+
+            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+            request.Method = "GET";
+
+            using (HttpWebResponse response = await request.GetResponseAsync() as HttpWebResponse)
+            {
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+
+                string result = await reader.ReadToEndAsync();
+
+                ViewBag.JSON = result;
+            }
+
+            return View();
+        }
+    }
+}
